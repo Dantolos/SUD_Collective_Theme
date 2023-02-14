8e1daf5c467aa6c9a28ec6781bc14e70c7c2d913
Dantolos <45291569+Dantolos@users.noreply.github.com>
2023-02-14 08:55:35 +0100

    0.1.11

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function boilerplate_load_assets() {
   wp_enqueue_script('ourmainjs', get_theme_file_uri('/build/index.js'), array('wp-element'), '1.0', true);
-  wp_enqueue_style('ourmaincss', get_theme_file_uri('/build/index.css', array(), '1.0.02'));
+  wp_enqueue_style('ourmaincss', get_theme_file_uri('/build/index.css', array(), '1.0.03'));
 }
 
 add_action('wp_enqueue_scripts', 'boilerplate_load_assets');
--- templates/archive-event.php
+++ templates/archive-event.php
@@ -11,7 +11,20 @@ $args = array(
     'post_status' => 'publish',
     'post_type' => 'event'
 );
-$events = new WP_Query($args);
+$eventObject = new WP_Query($args);
+
+$events = $eventObject->posts;
+
+if(!function_exists('event_order_by_date')){
+    function event_order_by_date($a, $b){
+        if( get_field( 'facts',  $a->ID )['date'] === get_field( 'facts',  $b->ID ) ){
+            return 0;
+        }
+        return get_field( 'facts',  $a->ID )['date'] > get_field( 'facts',  $b->ID )['date'] ? -1 : 1;
+    }
+}
+
+uasort($events, 'event_order_by_date');
 
 echo '<div class="event-page-container">';
 
@@ -20,7 +33,7 @@ echo the_content();
 echo '</div>';
 
 echo '<div class="event-card-container">';
-foreach($events->posts as $event){
+foreach($events as $event){
     $eventCard = new \sud\components\eventCard\Event_Card($event->ID);
     echo $eventCard->html;
 }
